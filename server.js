8bb250a16f44dc748bbe6a5f5b3b13b6b7340450
mellow0614 <mellow0614@naver.com>
2020-12-21 13:19:13 +0900

    1221

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(express.static(path.join(__dirname, '/web')));
 
-
+ 
 /************* Routing **************/
 
 //intro
